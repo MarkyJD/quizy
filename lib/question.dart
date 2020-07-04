8b2cc135e4e588b8d770b16870de597a1224624a
MarkyJD <marky.dodson@gmail.com>
2020-07-04 10:03:13 +1000

    Added helpers for questions and answers

---
+++ lib/question.dart
@@ -0,0 +1,18 @@
+class Question {
+  String _question;
+  List<String> _answers;
+  String _correctAnswer;
+
+  Question(String _question, List<String> _answers, String _correctAnswer) {
+    this._question = _question;
+    this._answers = _answers;
+    this._correctAnswer = _correctAnswer;
+  }
+
+  String get question => _question;
+  List<String> get answers => _answers;
+
+  bool isAnswerCorrect(String answer) {
+    return answer.compareTo(_correctAnswer) == 0 ? true : false;
+  }
+}
---
+++ lib/questionbank.dart
@@ -0,0 +1,102 @@
+import 'package:quizy/question.dart';
+
+class QuestionBank {
+  List<Question> _questionsList;
+  final List<List<String>> _questionsAndAnswers = [
+    [
+      'What file type is typically used to create layouts?',
+      '.pdf',
+      '.xml',
+      '.txt',
+      '.png',
+      '.xml'
+    ],
+    [
+      'What method is typically called when an app first starts up?',
+      'onClick()',
+      'onStart()',
+      'nextInt()',
+      'onCreate()',
+      'onCreate()'
+    ],
+    [
+      'What folder are images typically stored in?',
+      'values/',
+      'manifests/',
+      'layout/',
+      'drawable/',
+      'drawable/'
+    ],
+    [
+      'Which data type is NOT a valid data type?',
+      'pointer',
+      'int',
+      'boolean',
+      'double',
+      'pointer'
+    ],
+    [
+      'What xml version number is declared at the very beginning of every xml file?',
+      '2.0',
+      '1.0',
+      '3.0',
+      '1.6',
+      '1.0'
+    ],
+    [
+      'Which of the following is NOT an attribute of TextView?',
+      'text',
+      'textSize',
+      'textAlignment',
+      'shape',
+      'shape'
+    ],
+    [
+      'What is the correct way to insert inline comments?',
+      '// ...comment',
+      '# ...comment',
+      '<! ...comment',
+      '/* ...comment',
+      '// ...comment'
+    ],
+    [
+      'A loop inside the body of another loop is called _____',
+      'Nested',
+      'Loop-in-loop',
+      'Loop inception',
+      'Double loops',
+      'Nested'
+    ],
+    [
+      'Which statement is correct?',
+      'int myArray[] = 5;',
+      'int[] myArray = int[5];',
+      'int[] myArray = new int[5];',
+      'int[] myArray = [5];',
+      'int[] myArray = new int[5];'
+    ],
+    [
+      'What type of argument does the startActivity() method take?',
+      'Intent',
+      'Integer',
+      'View',
+      'String',
+      'Intent'
+    ],
+  ];
+
+  QuestionBank() {
+    _questionsList = [];
+    for (var s in _questionsAndAnswers) {
+      List<String> answers = [];
+      answers.add(s[1]);
+      answers.add(s[2]);
+      answers.add(s[3]);
+      answers.add(s[4]);
+      String correctAnswer = s[5];
+      _questionsList.add(new Question(s[0], answers, correctAnswer));
+    }
+  }
+
+  List<Question> get questionsList => _questionsList;
+}
